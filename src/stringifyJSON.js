bda36b76966c5a25aac4ce88fcde5df3aeef0a27
stellaliang21 <stellaliang2014@gmail.com>
2019-02-19 15:27:34 -0800

    solved

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,38 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  if (typeof obj === 'number') {
+    return String(obj);
+  }
+  if (obj === null) {
+    return 'null';
+  }
+  if (typeof obj === 'boolean') {
+    return String(obj);
+  }
+  if (typeof obj === 'string') {
+    return '"' + obj + '"';
+  }
+  if (Array.isArray(obj)) {
+    var arr = [];
+    for (var i = 0; i < obj.length; i++) {
+      arr.push(stringifyJSON(obj[i]));
+    }
+    return '[' + arr + ']';
+  }
+  if (typeof obj === 'object') {
+    var arr2 = [];
+    for (var key in obj) {
+      if (obj[key] === undefined) {
+        return '{}';
+      }
+      
+      arr2.push('"' + key + '"' + ':' + stringifyJSON(obj[key])); 
+    }
+    return '{' + arr2 + '}';
+  }
 };
+
+
+
+console.log(stringifyJSON({'a': 'apple'}));
