3d159f624ff3d38228c12ac4278b9438a6d08c9b
stellaliang21 <stellaliang2014@gmail.com>
2019-02-19 20:05:07 -0800

    solved 2

--- spec/getElementsByClassNameSpec.js
+++ spec/getElementsByClassNameSpec.js
@@ -11,16 +11,16 @@ var htmlStrings = [
 describe('getElementsByClassName', function() {
 
   it('should match the results of calling the built-in function', function() {
-    $('body').addClass('targetClassName');
-    htmlStrings.forEach(function(htmlString) {
-      var $rootElement = $(htmlString);
-      $('body').append($rootElement);
+    $('body').addClass('targetClassName'); // adding "targetClassName" as a class on the body element
+    htmlStrings.forEach(function(htmlString) { // for each html string above
+      var $rootElement = $(htmlString); // turn the string into a jQuery object (DOM node)
+      $('body').append($rootElement); // add the new DOM node to the body of the html (i.e., dom)
 
-      var result = getElementsByClassName('targetClassName');
-      var expectedNodeList = document.getElementsByClassName('targetClassName');
-      var expectedArray = Array.prototype.slice.apply(expectedNodeList);
-      var equality = _.isEqual(result, expectedArray); // why can't we use `===` here?
-      expect(equality).to.equal(FILL_ME_IN);
+      var result = getElementsByClassName('targetClassName'); // calling your function, for 'targetClassName'
+      var expectedNodeList = document.getElementsByClassName('targetClassName'); // Getting the expected result from calling the function you're emulating
+      var expectedArray = Array.prototype.slice.apply(expectedNodeList); // converts expectedNodeList to an array
+      var equality = _.isEqual(result, expectedArray); // why can't we use `===` here? 
+      expect(equality).to.equal(true);
 
       $rootElement.remove();
     });
--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,36 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
+var getElementsByClassName = function(className) {
   // your code here
+  var arr = [];
+  var searchNodes = function (nodes) {
+    if (nodes.classList && nodes.classList.contains(className)) {
+      arr.push(nodes);
+    }
+    if (nodes.childNodes) {
+      for (var i = 0; i < nodes.childNodes.length; i++) {
+        searchNodes(nodes.childNodes[i]);
+      }
+    }
+  };
+  searchNodes(document.body);
+  return arr;
 };
+
+
+//document.body, element.childNodes, and element.classList
+
+
+  //   for (var i = 0; i < nodes.length; i++) {
+  //     var node = nodes[i];
+  //     if (node.classList && node.classList.contains(className)) {
+  //       arr.push(node);
+  //     }
+  //   }
+  //   if (nodes.childNodes) {
+  //     searchNodes(nodes.childNodes);
+  //   }
+  // };
+  // searchNodes(document.body);
+  // return arr;
